41f4ecf5f60b2e54de4b817ba1147fc40d004f65
gyanaranjanpanda <gyanaranjanpanda16@gmail.com>
2026-03-15 08:06:01 +0530

    created  multi module project

---
+++ core/src/main/java/com/gyanaranjan/JavaLiveCoding2026/ReverseStringWithoutBuiltInMethod.java
@@ -0,0 +1,21 @@
+package com.gyanaranjan.JavaLiveCoding2026;
+
+import java.util.Arrays;
+
+public class ReverseStringWithoutBuiltInMethod {
+    public static void main(String[] args) {
+        String str = "gyanaranjan";
+        //without using built in method :-
+        char[] charArray = str.toCharArray();
+        //Using for loop :-
+        for(int i=charArray.length-1;i>=0;i--){
+            System.out.print(charArray[i]);
+        }
+        //Using Two Pointers:-
+
+
+
+
+
+    }
+}
